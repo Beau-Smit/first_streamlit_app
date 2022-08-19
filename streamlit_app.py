295b0dec0f616eedebd4409ffb9d8826b855f98a
Beau Smit <40701137+Beau-Smit@users.noreply.github.com>
2022-08-19 10:43:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + "kiwi")
 
-# write your own comment -what does the next line do? 
+# convert json into a dataframe
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# display the dataframe
 streamlit.dataframe(fruityvice_normalized)
